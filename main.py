e84c63a6a0508460156e1c4374ce3dc0e19f9bee
timchansky3994 <timurall@yandex.ru>
2021-02-03 22:00:41 +0300

    Added shooting mechanics

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 import os
 import pygame
+from math import sin, cos, radians
 
 NORTH, EAST, SOUTH, WEST = 0, 1, 2, 3
 FPS = 60
 pygame.init()
-clock = pygame.time.Clock()
-size = WIDTH, HEIGHT = 500, 500
+size = WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode(size)
 pygame.key.set_repeat(10, 20)
 
@@ -15,15 +15,19 @@ def terminate():
     pygame.quit()
     sys.exit()
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return
-        pygame.display.flip()
-        clock.tick(FPS)
+    # while True:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             terminate()
+    #         elif event.type == pygame.KEYDOWN or \
+    #                 event.type == pygame.MOUSEBUTTONDOWN:
+    #             return
+    #     pygame.display.flip()
+    #     clock.tick(FPS)
+
+
+def convert_dir_to_x_y(direction):
+    return int(sin(radians(direction * 90))), -int(cos(radians(direction * 90)))
 
 
 def load_image(name, colorkey=None):
@@ -53,12 +57,33 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, center_x, center_y, owner_name, direction, velocity=5):
+        super().__init__(top_layer_group, bullet_group,  all_sprites)
+        self.velocity = velocity
+        self.direction = direction
+        self.image = pygame.transform.rotate(bullet_image, -90 * self.direction)
+        self.rect = self.image.get_rect()
+        self.rect.center = center_x, center_y
+        if owner_name == 'Player':
+            self.enemies = enemy_team
+        else:
+            self.enemies = player_team
+
+    def update(self):
+        x, y = self.rect.x, self.rect.y
+        dir_x, dir_y = convert_dir_to_x_y(self.direction)
+        self.rect = self.rect.move(self.velocity * dir_x, self.velocity * dir_y)
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
+        super().__init__(player_team, top_layer_group, all_sprites)
         self.image = player_image
-        self.direction = 0  # 0 север 1 восток 2 юг 3 запад
+        self.direction = NORTH
         self.rect = self.image.get_rect().move(tile_width * pos_x + 4, tile_height * pos_y + 2)
+        self.cooldown_duration = 750
+        self.cooldown_start = -self.cooldown_duration
 
     def update(self, dx, dy, direction):
         self.rect = self.rect.move(dx, dy)
@@ -75,6 +100,18 @@ class Player(pygame.sprite.Sprite):
             old_center = self.rect.center
             self.rect = self.image.get_rect()
             self.rect.center = old_center
+
+    def shoot(self):
+        if pygame.time.get_ticks() - self.cooldown_start >= self.cooldown_duration:
+            dir_x, dir_y = convert_dir_to_x_y(self.direction)
+            bullet_radius = max(bullet_image.get_width(), bullet_image.get_height()) // 2
+            Bullet(self.rect.x + self.rect.width // 2 * (1 + dir_x) + bullet_radius * dir_x,
+                   self.rect.y + self.rect.height // 2 * (1 + dir_y) + bullet_radius * dir_y,
+                   self.__class__.__name__, self.direction)
+            self.cooldown_start = pygame.time.get_ticks()
+
+    def set_cooldown(self, cooldown):
+        self.cooldown_duration = cooldown
 
 
 # class Camera:
@@ -129,16 +166,20 @@ tile_images = {
     'block': load_image('block.png'),
     'empty': load_image('ground.png')}
 player_image = load_image('player_tank.png')
+bullet_image = load_image('bullet.png')
 tile_width = tile_height = 32
-player = None
 all_sprites = pygame.sprite.Group()
+top_layer_group = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 collidable_group = pygame.sprite.Group()
 breakable_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+player_team = pygame.sprite.Group()
+enemy_team = pygame.sprite.Group()
 player, level_x, level_y = generate_level(load_level('map1.txt'))
 pygame.display.set_caption("Танчики")
 # camera = Camera((level_x, level_y))
+clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get():
@@ -147,6 +188,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 terminate()
+
             if event.key == pygame.K_UP:
                 player.update(0, -1, NORTH)
             if event.key == pygame.K_RIGHT:
@@ -155,11 +197,15 @@ while running:
                 player.update(0, 1, SOUTH)
             if event.key == pygame.K_LEFT:
                 player.update(-1, 0, WEST)
+
+            if event.key == pygame.K_e:
+                player.shoot()
     screen.fill('black')
     # camera.update(player)
     # for sprite in all_sprites:
     #     camera.apply(sprite)
+    bullet_group.update()
     tiles_group.draw(screen)
-    player_group.draw(screen)
+    top_layer_group.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
